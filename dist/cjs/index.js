29eb7a96144f52ea76250968811ad292067cd5c1
Paolo Insogna <paolo@cowtech.it>
2021-01-04 14:44:22 +0100

    feat: Export as ESM.

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -82,5 +82,9 @@ exports.plugin = fastify_plugin_1.default(function (instance, options, done) {
     done();
 }, { name: 'fastify-print-routes' });
 exports.default = exports.plugin;
-module.exports = exports.plugin;
-Object.assign(module.exports, exports);
+// Fix CommonJS exporting
+/* istanbul ignore else */
+if (typeof module !== 'undefined') {
+    module.exports = exports.plugin;
+    Object.assign(module.exports, exports);
+}
